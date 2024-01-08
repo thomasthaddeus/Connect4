bd45d2a6468c5db08c8fabe73cfc8a62dab3fa0d
thomasthaddeus <thaddeus.r.thomas@gmail.com>
2024-01-08 06:14:08 -0700

    update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,10 @@
 # tests/__init__.py
 
-from .test_AI import M
-from .test_board import
-from  import
-from  import
-from  import
-from  import
-from  import
-from  import
-from  import
+from .test_AI import *
+from .test_board import *
+from .test_game_history import *
+from .test_game_statistics import *
+from .test_main import *
+from .test_network import *
+from .test_player import *
+from .test_save_load import *
